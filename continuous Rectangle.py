a82e1716fa85dffb87d42fed6e6ac66fa3c88760
Colvchap <colvin.chapman@students.olin.edu>
2017-03-07 23:31:22 -0500

    g

--- continuous Rectangle.py
+++ continuous Rectangle.py
@@ -183,13 +183,19 @@ class Player():
         self.y = self.y
         pygame.draw.rect(screen, self.color, [self.x, self.y, self.width, self.height])
 
-
     def jumps(self):
         self.velocity = -9
         self.fall = 'on'
 
 
 class Text():
+    def __init__(self, text, x_pos, y_pos, size, color):
+        self.text = text
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.size = size
+        self.color = color
+
     def __init__(self, text, x_pos, y_pos, size, color):
         self.text = text
         self.x_pos = x_pos
